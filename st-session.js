87e1d3bfca1c90aa342a7b1a9c206b3f1cdc9f1d
kstost <monogatree@gmail.com>
2020-04-29 17:30:58 +0000

    dd

--- st-session.js
+++ st-session.js
@@ -1,3 +1,131 @@
-const fs = require("fs")
-module.exports = function(){
-}
+const stcookie = require("st-cookie-parser")
+module.exports = (function (query, option) {
+    option.table = option.table ? option.table : 'st_session';
+    option.sessionid = option.sessionid ? option.sessionid : 'STSESSID';
+    option.max_age = option.max_age !== undefined ? option.max_age : 3600;
+    option.keep_lasting = option.keep_lasting !== undefined ? option.keep_lasting : true;
+    option.basics = option.basics ? option.basics : {
+        Path: '/',
+        SameSite: 'Lax',
+        HttpOnly: true,
+        Secure: true,
+    };
+
+    function create_table() {
+        return (async () => {
+            let query_ = "CREATE TABLE `" + option.table + "` ( " +
+                "  `session_id` varchar(128) NOT NULL default '', " +
+                "  `expires` int(11), " +
+                "  `data` mediumtext, " +
+                "  PRIMARY KEY (`session_id`) " +
+                ")";
+            return await query(query_);
+        })();
+    }
+
+    function makeid(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    function create_new_session(req, res, value) {
+        return (async () => {
+            let sessid = makeid(40);
+            while (true) {
+                result = await query("insert into " + option.table + " (session_id, expires, data) values (?,?,?)", [sessid, option.max_age + timestamp(), JSON.stringify(value)]);
+                let error = result.errno && result.constructor.name === 'Error';
+                if (!error) {
+                    let cookie = get_cookie(req);
+                    cookie[option.sessionid] = sessid;
+                    req.headers.cookie = stcookie.stringify(cookie);
+                    let newCookie = {
+                        [option.sessionid]: sessid,
+                        'Max-Age': option.max_age,
+                        ...option.basics,
+                    };
+                    if (option.keep_lasting) {
+                        delete newCookie['Max-Age'];
+                    }
+                    res.set('Set-Cookie', stcookie.stringify(newCookie));
+                    break;
+                } else {
+                    if (result.errno === 1146) {
+                        await create_table();
+                    }
+                    if (result.errno === 1062) {
+                        sessid = makeid(40);
+                    }
+                }
+            }
+            return sessid;
+        })();
+    }
+
+    function get_cookie(req) {
+        let cookie = req.headers.cookie ? stcookie.parse(req.headers.cookie) : {};
+        return cookie;
+    }
+
+    function get_sessid(req) {
+        let sessid = get_cookie(req)[option.sessionid];
+        if (sessid === undefined) {
+            sessid = '';
+        }
+        return sessid;
+    }
+
+    function timestamp() {
+        return Math.round(new Date().getTime() / 1000);
+    }
+
+    return {
+        get_attributes(req, res) {
+            return (async () => {
+                let query_ = "";
+                let result = await query("select * from " + option.table + " where session_id = ?", [get_sessid(req)]);
+                let error = result.errno && result.constructor.name === 'Error';
+                if (!error && result.length === 1) {
+                    let sess = result[0];
+                    let data = JSON.parse(sess.data);
+                    let exp = Number(sess.expires) < timestamp();
+                    if (exp) {
+                        await query("delete from " + option.table + " where session_id = ?", [get_sessid(req)]);
+                        await create_new_session(req, res, data);
+                    } else {
+                    }
+                    return data;
+                } else {
+                    if (error && result.errno === 1146) {
+                        await create_table();
+                    }
+                    return {};
+                }
+            })();
+        },
+        set_attributes(req, res, value, overwrite) {
+            return (async () => {
+                if (get_sessid(req) === '') {
+                    await create_new_session(req, res, value);
+                } else {
+                    if (!overwrite) {
+                        let pre = await this.get_attributes(req, res);
+                        value = {
+                            ...pre,
+                            ...value
+                        };
+                    }
+                    let result = await query("update " + option.table + " set data=? where session_id=?", [JSON.stringify(value), get_sessid(req)]);
+                    if (!result.affectedRows || result.constructor.name === 'Error') {
+                        await create_new_session(req, res, value);
+                    }
+                }
+                return value;
+            })();
+        },
+    }
+});
